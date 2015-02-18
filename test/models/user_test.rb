684fe94d656d0a74149c991c75c7d397feae128d
Adam McLean <adam.mclean1@gmail.com>
2015-02-17 22:28:37 -0800

    create user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,11 @@ class UserTest < ActiveSupport::TestCase
   def setup
   	@user = User.new(name: "Adam", email: "adam.mclean1@gmail.com", password: "foobar", password_confirmation: "foobar")
   end
+
+  test "email addresses should be saved as lower-case" do
+  	mixed_case_email = "Foo@ExAMPLE.CoM"
+  	@user.email = mixed_case_email
+  	@user.save
+  	assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
