e207e7e21bfd806d9c83bf17308c44484cf8e3ea
Adam McLean <adam.mclean1@gmail.com>
2015-02-20 07:41:53 -0800

    add remember cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to the Brogrammr community!"
+    	log_in @user
+      flash[:success] = "Welcome to the Brogrammr community!"
     	redirect_to @user
       # Handle a successful save.
 		else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-	 has_many :post
+	attr_accessor :remember_token
+  has_many :post
 	 
-	 before_save { self.email = email.downcase }
+	before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -9,4 +10,23 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+￼   
+  BCrypt::Password.create(string, cost: cost)
+  
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+    
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end 
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
