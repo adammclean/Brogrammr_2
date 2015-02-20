d4718d790db0745e613bf8dba2e2fce2b3ec3c68
Adam McLean <adam.mclean1@gmail.com>
2015-02-20 12:32:21 -0800

    finish ish site authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 
 class UsersController < ApplicationController
  before_action :logged_in_user, only: [:edit, :update]
-
+￼before_action :correct_user, only: [:edit, :update]￼
   def show
   	@user = User.find(params[:id])
   end
@@ -50,4 +50,10 @@ class UsersController < ApplicationController
           redirect_to login_url
       end 
   end
+
+  def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+  end
+
 end
